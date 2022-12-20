9465881413c536b37b5fb576e33335b3ea3b2efd
Yauheni Dubovik <ydubovik@exadel.com>
2022-12-20 10:26:38 +0100

    changed module.css to @emotion/styled

--- src/components/DishItem/DishItem.tsx
+++ src/components/DishItem/DishItem.tsx
@@ -1,17 +1,39 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { Dish } from "../../types/Dish";
 import FullRecipe from "../FullRecipe/FullRecipe";
 import classes from "./DishItem.module.css";
 
+const DishContainer = styled.div`
+  display: flex;
+  padding: 15px;
+  border: 2px solid teal;
+  margin: 15px 0;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const DishName = styled.h2`
+  text-align: center;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  // flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
 const DishItem = (dish: Dish) => {
   return (
-    <div className={classes.dish_item_container}>
-      <div className={classes.dish_header}>
-        <h1 className={classes.dish_name}>{dish.name}</h1>
+    <DishContainer>
+      <Header>
+        <DishName>{dish.name}</DishName>
         <img src={`${dish.img}`} className={classes.dish_image} />
-      </div>
+      </Header>
       <FullRecipe dish={dish} limit={100} />
-    </div>
+    </DishContainer>
   );
 };
 
